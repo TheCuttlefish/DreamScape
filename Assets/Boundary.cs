4105cfc91468a3be593d3b59f10d7aae53d8499f
Cuttlefish <zh4n@live.co.uk>
2018-06-09 21:55:15 +0100

    Island Scene

---
+++ Assets/Boundary.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundary : MonoBehaviour {
+
+	// Use this for initialization
+	public bool enableOnStart = true;
+	void Start () {
+		GetComponent<MeshRenderer> ().enabled = enableOnStart;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
