b1f81ba36359552970701ab6ec5a14b3f373e079
Cuttlefish <zh4n@live.co.uk>
2018-08-13 23:36:54 +0100

    Scene update

---
+++ Assets/_Scenes/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	Vector3 playerPos = new Vector3 (1, 0, 0);
+	Vector3 enemyPos = new Vector3 (-1, 0, 0);
+
+	void Start () {
+
+		print (Vector3.Dot (playerPos.normalized, enemyPos.normalized));
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		float sineFun = Mathf.Sin (Time.time);
+		print (sineFun);
+
+	}
+}
