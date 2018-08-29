b31d997538276f9030afc633003174b283e3e345
Cuttlefish <zh4n@live.co.uk>
2018-08-29 01:16:17 +0100

    Added Tumbleweeds

---
+++ Assets/_Fields/Tumbleweed.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tumbleweed : MonoBehaviour {
+
+	public float speed = 0.1f;
+	int timer = 0;
+	// Update is called once per frame
+	void Update () {
+
+		timer++;
+		if (timer > 100) {
+
+			timer = Random.RandomRange (0, 99);
+			GetComponent<Rigidbody> ().AddForce (Vector3.up * speed);
+		}
+
+		
+	}
+}
